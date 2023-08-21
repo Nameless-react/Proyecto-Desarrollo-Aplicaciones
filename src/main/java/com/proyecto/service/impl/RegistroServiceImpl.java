7763d8860316b167b38cd298ddc101459e1afddb
Berlincordero <114275282+Berlincordero@users.noreply.github.com>
2023-08-20 22:19:23 -0600

    registro

--- src/main/java/com/proyecto/service/impl/RegistroServiceImpl.java
+++ src/main/java/com/proyecto/service/impl/RegistroServiceImpl.java
@@ -1,14 +1,15 @@
-
 package com.proyecto.service.impl;
 
 /**
  *
  * @author enriq
  */
-
 import com.proyecto.domain.Usuario;
 import com.proyecto.dao.ClienteDao;
 import com.proyecto.dao.EmpleadoDao;
+import com.proyecto.domain.Cliente;
+import com.proyecto.domain.Empleado;
+import com.proyecto.service.ClienteService;
 import com.proyecto.service.CorreoService;
 import com.proyecto.service.EmpleadoService;
 import com.proyecto.service.RegistroService;
@@ -34,121 +35,126 @@ public class RegistroServiceImpl implements RegistroService {
     private MessageSource messageSource;  //creado en semana 4...
     @Autowired
     private FireBaseStorageServiceImpl firebaseStorageService;
+    @Autowired
+    private EmpleadoService empleadoService;
+    @Autowired
+    private ClienteService clienteService;
 
     @Override
     public Model activar(Model model, String username, String clave) {
-        Usuario usuario = 
-                EmpleadoService.findByUsernameAndPassword(username, Password);
-        if (usuario != null) {
-            model.addAttribute("usuario", usuario);
+        Cliente cliente = clienteService.findByUsernameAndPassword(username, clave);
+        Empleado empleado = empleadoService.findByUsernameAndPassword(username, clave);
+
+        if (cliente != null) {
+            model.addAttribute("usuario", cliente);
+
+        } else if (empleado != null) {
+            model.addAttribute("usuario", empleado);
         } else {
             model.addAttribute(
-                    "titulo", 
-                    messageSource.getMessage(
-                            "registro.activar", 
-                            null,  Locale.getDefault()));
+                    "titulo",
+                    "usuario no se puede activar");
+
             model.addAttribute(
-                    "mensaje", 
-                    messageSource.getMessage(
-                            "registro.activar.error", 
-                            null, Locale.getDefault()));
+                    "mensaje",
+                    "el usuario no se puedo activar debido a un error");
+
+            return model;
         }
-        return model;
-    }
 
-    @Override
-    public void activar(Usuario usuario, MultipartFile imagenFile) {
-        var codigo = new BCryptPasswordEncoder();
-        usuario.setPassword(codigo.encode(usuario.getPassword()));
-
-        if (!imagenFile.isEmpty()) {
-            usuarioService.save(usuario, false);
-            usuario.setRutaImagen(
-                    firebaseStorageService.cargaImagen(
-                            imagenFile, 
-                            "usuarios", 
-                            usuario.getIdUsuario()));
-        }
-        usuarioService.save(usuario, true);
-    }
-
-    @Override
-    public Model crearUsuario(Model model, Usuario usuario) 
+//    @Override
+//    public void activar(Usuario usuario, MultipartFile imagenFile) {
+//        var codigo = new BCryptPasswordEncoder();
+//        usuario.setPassword(codigo.encode(usuario.getPassword()));
+//
+//        if (!imagenFile.isEmpty()) {
+//            usuarioService.save(usuario, false);
+//            usuario.setRutaImagen(
+//                    firebaseStorageService.cargaImagen(
+//                            imagenFile, 
+//                            "usuarios", 
+//                            usuario.getIdUsuario()));
+//        }
+//        usuarioService.save(usuario, true);
+//    }
+        @Override
+        public Model crearUsuario
+        (Model model, Usuario usuario) 
             throws MessagingException {
-        String mensaje;
-        if (!usuarioService.
-                existeUsuarioPorUsernameOCorreo(
-                        usuario.getUsername(), 
-                        usuario.getCorreo())) {
-            String clave = demeClave();
-            usuario.setPassword(clave);
-            usuario.setActivo(false);
-            usuarioService.save(usuario, true);
-            enviaCorreoActivar(usuario, clave);
-            mensaje = String.format(
+            String mensaje;
+            if (usuarioService.finByUsernameAndEmail(usuario.getUsername(), usuario.getEmail()) == null) {
+                String clave = demeClave();
+                usuario.setPassword(clave);
+                usuario.setActivo(false);
+                usuarioService.save(usuario, true);
+                enviaCorreoActivar(usuario, clave);
+                mensaje = String.format(
+                        messageSource.getMessage(
+                                "registro.mensaje.activacion.ok",
+                                null,
+                                Locale.getDefault()),
+                        usuario.getCorreo());
+            } else {
+                mensaje = String.format(
+                        messageSource.getMessage(
+                                "registro.mensaje.usuario.o.correo",
+                                null,
+                                Locale.getDefault()),
+                        usuario.getUsername(), usuario.getCorreo());
+            }
+            model.addAttribute(
+                    "titulo",
                     messageSource.getMessage(
-                            "registro.mensaje.activacion.ok", 
-                            null, 
-                            Locale.getDefault()),
-                    usuario.getCorreo());
-        } else {
-            mensaje = String.format(
-                    messageSource.getMessage(
-                            "registro.mensaje.usuario.o.correo", 
-                            null, 
-                            Locale.getDefault()),
-                    usuario.getUsername(), usuario.getCorreo());
+                            "registro.activar",
+                            null,
+                            Locale.getDefault()));
+            model.addAttribute(
+                    "mensaje",
+                    mensaje);
+            return model;
         }
-        model.addAttribute(
-                "titulo", 
-                messageSource.getMessage(
-                        "registro.activar", 
-                        null, 
-                        Locale.getDefault()));
-        model.addAttribute(
-                "mensaje", 
-                mensaje);
-        return model;
-    }
 
-    @Override
-    public Model recordarUsuario(Model model, Usuario usuario) 
+        @Override
+        public Model recordarUsuario
+        (Model model, Usuario usuario) 
             throws MessagingException {
-        String mensaje;
-        Usuario usuario2 = usuarioService.getUsuarioPorUsernameOCorreo(
-                usuario.getUsername(), 
-                usuario.getCorreo());
-        if (usuario2 != null) {
-            String clave = demeClave();
-            usuario2.setPassword(clave);
-            usuario2.setActivo(false);
-            usuarioService.save(usuario2, false);
-            enviaCorreoRecordar(usuario2, clave);
-            mensaje = String.format(
-                    messageSource.getMessage(
-                            "registro.mensaje.recordar.ok", 
-                            null, 
-                            Locale.getDefault()),
+            String mensaje;
+            Usuario usuario2 = usuarioService.getUsuarioPorUsernameOCorreo(
+                    usuario.getUsername(),
                     usuario.getCorreo());
-        } else {
-            mensaje = String.format(
+            if (usuario2 != null) {
+                String clave = demeClave();
+                usuario2.setPassword(clave);
+                usuario2.setActivo(false);
+                usuarioService.save(usuario2, false);
+                enviaCorreoRecordar(usuario2, clave);
+                mensaje = String.format(
+                        messageSource.getMessage(
+                                "registro.mensaje.recordar.ok",
+                                null,
+                                Locale.getDefault()),
+                        usuario.getCorreo());
+            } else {
+                mensaje = String.format(
+                        messageSource.getMessage(
+                                "registro.mensaje.usuario.o.correo",
+                                null,
+                                Locale.getDefault()),
+                        usuario.getUsername(), usuario.getCorreo());
+            }
+            model.addAttribute(
+                    "titulo",
                     messageSource.getMessage(
-                            "registro.mensaje.usuario.o.correo", 
-                            null, 
-                            Locale.getDefault()),
-                    usuario.getUsername(), usuario.getCorreo());
+                            "registro.activar",
+                            null,
+                            Locale.getDefault()));
+            model.addAttribute(
+                    "mensaje",
+                    mensaje);
+            return model;
         }
-        model.addAttribute(
-                "titulo", 
-                messageSource.getMessage(
-                        "registro.activar", 
-                        null, 
-                        Locale.getDefault()));
-        model.addAttribute(
-                "mensaje", 
-                mensaje);
-        return model;
-    }
+
+    
 
     private String demeClave() {
         String tira = "ABCDEFGHIJKLMNOPQRSTUXYZabcdefghijklmnopqrstuvwxyz0123456789_*+-";
@@ -165,32 +171,47 @@ public class RegistroServiceImpl implements RegistroService {
 
     private void enviaCorreoActivar(Usuario usuario, String clave) throws MessagingException {
         String mensaje = messageSource.getMessage(
-                "registro.correo.activar", 
+                "registro.correo.activar",
                 null, Locale.getDefault());
         mensaje = String.format(
-                mensaje, usuario.getNombre(), 
-                usuario.getApellidos(), servidor, 
+                mensaje, usuario.getNombre(),
+                usuario.getApellidos(), servidor,
                 usuario.getUsername(), clave);
         String asunto = messageSource.getMessage(
-                "registro.mensaje.activacion", 
+                "registro.mensaje.activacion",
                 null, Locale.getDefault());
         correoService.enviarCorreoHtml(usuario.getCorreo(), asunto, mensaje);
     }
 
     private void enviaCorreoRecordar(Usuario usuario, String clave) throws MessagingException {
         String mensaje = messageSource.getMessage(""
-                + "registro.correo.recordar", 
-                null, 
+                + "registro.correo.recordar",
+                null,
                 Locale.getDefault());
         mensaje = String.format(
-                mensaje, usuario.getNombre(), 
-                usuario.getApellidos(), servidor, 
+                mensaje, usuario.getNombre(),
+                usuario.getApellidos(), servidor,
                 usuario.getUsername(), clave);
         String asunto = messageSource.getMessage(
-                "registro.mensaje.recordar", 
+                "registro.mensaje.recordar",
                 null, Locale.getDefault());
         correoService.enviarCorreoHtml(
-                usuario.getCorreo(), 
+                usuario.getCorreo(),
                 asunto, mensaje);
+    }
+
+    @Override
+    public Model crearUsuario(Model model, Usuario usuario) throws MessagingException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void activar(Usuario usuario, MultipartFile imagenFile) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Model recordarUsuario(Model model, Usuario usuario) throws MessagingException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
